4e977ec6040b87791804ddc7ae732d75390ded3b
Felipe <Felipe@FELIPE-PC>
2023-09-02 20:29:24 -0300

    correcao projeto

--- src/main/java/com/felipe/DoadorSangueAPI/DoadorSangueApplication.java
+++ src/main/java/com/felipe/DoadorSangueAPI/DoadorSangueApplication.java
@@ -1,19 +1,16 @@
 package com.felipe.DoadorSangueAPI;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "DoadorSangue API", version = "1.0", description = "Api para computação de dados de doadores de sangue"))
-public class ImcApplication {
+public class DoadorSangueApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ImcApplication.class, args);
+		SpringApplication.run(DoadorSangueApplication.class, args);
 	}
 
 }
